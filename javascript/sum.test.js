df7ba887cd88ef33ddedda2c48b9454643fef647
MPSBeats <145999481+MPSBeats@users.noreply.github.com>
2024-02-11 16:47:04 +0100

    Update sum.test.js

--- javascript/sum.test.js
+++ javascript/sum.test.js
@@ -1,4 +1,4 @@
-const test1 = require('./'); // Importation de la fonction depuis le même répertoire
+const affichageProduit = require('./javascript/'); // Importation de la fonction depuis le même répertoire
 
 //Les tests viendront ici
 

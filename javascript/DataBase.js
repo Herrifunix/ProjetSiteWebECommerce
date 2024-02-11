9e1d2555526509257570729239c850ee6dbd41d3
Drx93 <145999414+Drx93@users.noreply.github.com>
2024-02-11 16:21:09 +0100

    Add files via upload

---
+++ javascript/DataBase.js
@@ -0,0 +1,95 @@
+//déclaration de la Database
+const response = [
+    {
+        _id: "a001",
+        nomProduit : "nom produit 1",
+        description : "description 1",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4 ouu"
+        ],
+        prix: 15000
+
+    },
+    {
+        _id: "a002",
+        nomProduit : "nom produit 2",
+        description : "description 2",
+        options:[
+            "option1",
+            "option2",
+            "option3"
+        ],
+        prix: 2
+
+    },
+    {
+        _id: "a003",
+        nomProduit : "nom produit 3",
+        description : "description 3",
+        options:[
+            "option1",
+            "option2"
+        ],
+        prix: 3
+
+    },
+    {
+        _id: "a004",
+        nomProduit : "nom produit 4",
+        description : "description 4",
+        options:[
+            "option1",
+            "option2"
+        ],
+        prix: 4
+
+    },
+    {
+        _id: "a005",
+
+        nomProduit : "nom produit 5",
+        description : "description 5",
+        options:[
+            "option1",
+            "option2"
+        ],
+        prix: 5
+
+    },
+    {
+        _id: "a006",
+        nomProduit : "nom produit 6",
+        description : "description 6",
+        options:[
+            "option1",
+            "option2"
+        ],
+        prix: 6
+
+    },
+    {
+        _id: "a007",
+        nomProduit : "nom produit 7",
+        description : "description 7",
+        options:[
+            "option1",
+            "option2"
+        ],
+        prix: 7
+
+    },
+    {
+        _id: "a008",
+        nomProduit : "nnoke",
+        description : "dazegheryjheaztey",
+        options:[
+            "option1",
+            "option2"
+        ],
+        prix: 7853
+
+    },
+];

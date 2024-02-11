5a34b296c4c8d70d4bbc80186c0d7a28ea2063c4
Bastien Puyou Filipic <bastien.puyou.filipic@gmail.com>
2024-02-11 14:59:11 +0100

    small changes, debugged id

--- javascript/DataBase.js
+++ javascript/DataBase.js
@@ -3,16 +3,19 @@ import { Product } from "../javascript/Product.js";
 //déclaration de la Database
 export const response = [];
 
+// pour init a la main la database :
+// -> commenter l'appel d'initdatabase
+// -> rentrer les données a la main avec [addNewProduct(...,...,...,...,...),addNewProduct(),...]
 
 // construction d'une database
 function initDataFast(price, desc, img, name=""){
-    let key = "a" + response.length
-    for (let j=1; j < 3; j++){
+    for (let j=1; j < 10; j++){
+        let key = "a" + response.length
         if (name == ""){
-            name = key
+            let reName = key;
         };
-        addNewProduct(key,name,price,desc,img);
-    }
+        addNewProduct(key, reName, price, desc, img);
+    };
 };
 
 function addNewProduct(id, name, price, desc, img){
@@ -23,6 +26,7 @@ function createNewProduct(id, name, price, desc, img){
     return new Product(id, name, price, desc, img);
 };
 
-initDataFast(1,"desc","img",)
+// commenter la ligne du dessous dans le cas où on ne veut plus init automatiquement
+initDataFast(1,"desc","img",);
 
 console.log(response);

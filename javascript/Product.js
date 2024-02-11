66a9c86feda1319b0bd2151c1a4c5da6e6cdc3f1
Bastien Puyou Filipic <bastien.puyou.filipic@gmail.com>
2024-02-11 14:18:31 +0100

    erged Dorian's database with mine

--- javascript/Product.js
+++ javascript/Product.js
@@ -1,22 +1,26 @@
-export default class Product{
+export class Product{
     
-    constructor(name, price, description, imageLink){
+    constructor(id, name, price, description, imageLink){
+        this.id = id
         this.name = name;
         this.price = price;
         this.description = description;
         this.imageLink = imageLink;
-    }
+    };
 
     get getName(){
         return this.name;
-    }
+    };
     get getprice(){
         return this.price;
-    }
+    };
     get getDescription(){
         return this.description;
-    }
+    };
     get getImage(){
         return this.imageLink;
-    }
-}
+    };
+    get getId(){
+        return this.id;
+    };
+};

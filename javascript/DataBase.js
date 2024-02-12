bf87ed6714854950bd442cb8dc38956aa8ddff42
MPSBeats <145999481+MPSBeats@users.noreply.github.com>
2024-02-12 20:16:45 +0100

    Add files via upload

--- javascript/DataBase.js
+++ javascript/DataBase.js
@@ -12,6 +12,7 @@ const response = [
     },
     {
         _id: "c002",
+        img:"images/vélo.jpg",
         nomProduit : "Vélo Citadin",
         description : "Découvrez la polyvalence urbaine avec notre Vélo Citadin : cadre léger, géométrie confortable, transmission fluide, freins fiables, pneus adaptés à la ville. Parfait pour tous, commandez maintenant pour une expérience de conduite urbaine sans égale !",
         options:[
@@ -22,6 +23,7 @@ const response = [
     },
     {
         _id: "a003",
+        img:"images/foot1.JPG",
         nomProduit : "Ballon Classique",
         description : "Élevez votre passion pour le football avec notre Ballon Classique : design intemporel, motifs hexagonaux, revêtement durable, poids équilibré, pour une expérience de jeu exceptionnelle. Parfait pour tous les amateurs !",
         options:[
@@ -32,6 +34,7 @@ const response = [
     },
     {
         _id: "a004",
+        img:"images/foot2.JPG",
         nomProduit : "Ballon Professionnel",
         description : "Sublimez votre jeu avec notre Ballon de Football Professionnel : conçu pour les exigences les plus élevées, revêtement haute performance, coutures renforcées et trajectoire précise. Adoptez l'excellence sur le terrain avec ce ballon d'élite. Parfait pour les professionnels exigeants.",
         options:[
@@ -42,7 +45,7 @@ const response = [
     },
     {
         _id: "a005",
-
+        img:"images/piscine.jpg",
         nomProduit : "Bonnet de Bain",
         description : "Optez pour l'élégance aquatique avec notre Bonnet de Bain Classique : conçu pour une étanchéité optimale, matériau souple et confortable, ajustement parfait pour tous les types de cheveux. Que vous soyez un nageur chevronné ou un amateur, ce bonnet allie fonctionnalité et style intemporel.",
         options:[
@@ -53,6 +56,7 @@ const response = [
     },
     {
         _id: "a006",
+        img:"images/alter.jpg",
         nomProduit : "Set d'haltère 10Kg",
         description : "Équipez-vous de notre Set d'Haltères 10 kg : parfait pour renforcer votre entraînement à domicile, ces haltères en fonte solide offrent une prise confortable et des disques robustes. Idéal pour une variété d'exercices, ce set polyvalent s'adapte à tous les niveaux de fitness.",
         options:[
@@ -63,6 +67,7 @@ const response = [
     },
     {
         _id: "a007",
+        img:"images/elastic.jpg",
         nomProduit : "Elastiques",
         description : "Intensifiez votre entraînement avec notre Ensemble d'Élastiques Gymniques : fabriqués avec des matériaux durables et résistants, ces élastiques offrent une variété de niveaux de résistance pour répondre à tous les besoins d'entraînement. Polyvalents et compacts, ils sont parfaits pour tonifier et renforcer chaque groupe musculaire, que vous soyez débutant ou expert en fitness.",
         options:[
@@ -73,6 +78,7 @@ const response = [
     },
     {
         _id: "a008",
+        img:"images/haltère.JPG",
         nomProduit : "Paire d'Haltère 50kg",
         description : "Forgez votre force avec notre Paire d'Haltères 50 kg : conçue pour les athlètes sérieux et les amateurs de musculation, cette paire d'haltères en fonte offre une résistance intense et une construction robuste. Avec une prise antidérapante et des disques de poids précis, ces haltères sont l'outil parfait pour des séances d'entraînement variées et exigeantes",
         options:[

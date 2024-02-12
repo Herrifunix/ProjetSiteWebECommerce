f4e1a82b7a0f79ee3c70ff891f6ec19ecf1c6ee9
Drx93 <145999414+Drx93@users.noreply.github.com>
2024-02-13 00:58:58 +0100

    Delete ProjetSiteWebECommerce-main directory
    delete

--- ProjetSiteWebECommerce-main/javascript/sum.test.js
+++
@@ -1,7 +0,0 @@
-const tot = require(''); // Importation de la fonction depuis le même répertoire
-const tot = require(''); // Importation de la fonction depuis le même répertoire
-const tot = require(''); // Importation de la fonction depuis le même répertoire
-const tot = require(''); // Importation de la fonction depuis le même répertoire
-
-
-//Les tests viendront ici

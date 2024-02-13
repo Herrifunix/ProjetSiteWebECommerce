0b115f81c6b09ac0e8ae3887b8850c864f75242f
Drx93 <145999414+Drx93@users.noreply.github.com>
2024-02-13 01:00:07 +0100

    Add files via upload

--- javascript/sum.test.js
+++ javascript/sum.test.js
@@ -1,6 +1,7 @@
-const affichageProduit = require('./javascript/'); // Importation de la fonction depuis le même répertoire
+const tot = require(''); // Importation de la fonction depuis le même répertoire
+const tot = require(''); // Importation de la fonction depuis le même répertoire
+const tot = require(''); // Importation de la fonction depuis le même répertoire
+const tot = require(''); // Importation de la fonction depuis le même répertoire
+
 
 //Les tests viendront ici
-
-
-const test2 = require('./');
